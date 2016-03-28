70f094cbc3195372e75bf5e4a6d5afa69d6ebb76
mrchadley <marc.tremblay@uoit.net>
2016-03-28 12:50:29 -0400

    trying to get directories to list - Marc

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,4 +1,37 @@
 package client;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+
+import java.io.File;
+import java.util.LinkedList;
+
+public class Controller
+{
+    @FXML private TableView<File> clientView;
+    @FXML private TableView<File> serverView;
+
+    private File clientDir = new File(".");
+    private File serverDir = new File(".");
+
+    public void initialize()
+    {
+        DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setInitialDirectory(new File("."));
+        clientDir = chooser.showDialog(null);
+
+        System.out.println(clientDir.toString());
+
+        DirectoryChooser chooser2 = new DirectoryChooser();
+        chooser2.setInitialDirectory(new File("."));
+        serverDir = chooser2.showDialog(null);
+
+        System.out.println(serverDir.toString());
+
+        clientView.getItems().addAll(clientDir.listFiles());
+        serverView.getItems().addAll(serverDir.listFiles());
+    }
 }
