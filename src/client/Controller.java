9dcec9e2f9952f1cb8ea32c8b8aa62079ffa31eb
austinolig <austinoligario@gmail.com>
2016-03-28 14:18:27 -0400

    Populating List View - Austin

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,21 +1,28 @@
 package client;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
 import java.util.LinkedList;
 
 public class Controller
 {
-    @FXML private TableView<File> clientView;
-    @FXML private TableView<File> serverView;
+    @FXML private ListView<String> clientView;
+    @FXML private ListView<String> serverView;
+
 
     private File clientDir = new File(".");
     private File serverDir = new File(".");
+    public String filenameClient;
+    public String filenameServer;
 
     public void initialize()
     {
@@ -31,7 +38,22 @@ public class Controller
 
         System.out.println(serverDir.toString());
 
-        clientView.getItems().addAll(clientDir.listFiles());
-        serverView.getItems().addAll(serverDir.listFiles());
+
+        File[] filesC = clientDir.listFiles();
+        for (File file : filesC) {
+            if ( (file.isDirectory() == false) && (file.getAbsolutePath().endsWith(".txt") )) {
+                filenameClient = file.toString().substring(filenameClient.lastIndexOf("/") + 1);
+                clientView.getItems().addAll(filenameClient);
+            }
+        }
+
+        File[] filesS = serverDir.listFiles();
+        for (File file : filesS) {
+            if ( (file.isDirectory() == false) && (file.getAbsolutePath().endsWith(".txt") )) {
+                filenameServer = file.toString().substring(filenameServer.lastIndexOf("/") + 1);
+                serverView.getItems().addAll(filenameServer);
+            }
+        }
+
     }
 }
