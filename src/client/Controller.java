aa9c78a10540f7c7b70eec71920a91b9142ed7db
austinolig <austinoligario@gmail.com>
2016-03-28 16:38:46 -0400

    Added small function to controller and removed Client.java - Austin

--- src/client/Controller.java
+++ src/client/Controller.java
@@ -1,13 +1,20 @@
 package client;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.HashMap;
 import java.util.LinkedList;
 
@@ -24,15 +31,40 @@ public class Controller
     private File clientDir = new File(".");
     private File serverDir = new File(".");
 
+    Socket socket;
+
+    //Socket socket = new Socket();
+    //@FXML private Button upload;
+
+
+    public void upload(ActionEvent actionEvent) throws IOException{
+        socket = new Socket("127.0.0.1", 8080);
+        PrintWriter out = new PrintWriter(socket.getOutputStream());
+        String selectedFile = clientView.getSelectionModel().getSelectedItem();
+        out.println("UPLOAD " + selectedFile);
+    }
+
+
+    public void download(ActionEvent actionEvent){
+
+    }
+
     public void initialize()
     {
+        try {
+            socket = new Socket("127.0.0.1", 8080);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         DirectoryChooser chooser = new DirectoryChooser();
+        chooser.setTitle("Select the client directory...");
         chooser.setInitialDirectory(new File("."));
         clientDir = chooser.showDialog(null);
 
         System.out.println(clientDir.toString());
 
         DirectoryChooser chooser2 = new DirectoryChooser();
+        chooser2.setTitle("Select the server directory...");
         chooser2.setInitialDirectory(new File("."));
         serverDir = chooser2.showDialog(null);
 
--- src/client/Main.java
+++ src/client/Main.java
@@ -1,17 +1,24 @@
 package client;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("client.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("File Sharer");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
