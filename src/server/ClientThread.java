56f6818ee63febdcf5179623ca546bc15d31ef13
mrchadley <marc.tremblay@uoit.net>
2016-03-28 16:27:05 -0400

    server seems to maybe be working... but it's not tested - Marc

---
+++ src/server/ClientThread.java
@@ -0,0 +1,123 @@
+package server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class ClientThread extends Thread
+{
+    private Socket clientSocket;
+    private BufferedReader input;
+    private File serverDirectory;
+
+    public ClientThread(Socket connection, File serverDirectory)
+    {
+        super(connection.toString());
+        clientSocket = connection;
+        this.serverDirectory = serverDirectory;
+    }
+
+    @Override public void run()
+    {
+        handleCommand();
+    }
+
+    private void handleCommand()
+    {
+        try {
+            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            String line = input.readLine();
+            String[] args = line.split(" ");
+
+            if (args[0].intern() == "dir")
+            {
+                displayDirectory();
+            }
+            else if (args[0].intern() == "upload")
+            {
+                uploadFile(args[1]);
+            }
+            else if (args[0].intern() == "download")
+            {
+                downloadFile(args[1]);
+            }
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();;
+        }
+
+    }
+
+
+    private void displayDirectory()
+    {
+        try
+        {
+            input.close();
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+
+            for(File file : serverDirectory.listFiles())
+            {
+                if(file.isFile() && file.getName().endsWith(".txt"));
+                out.println(file.getName());
+            }
+            out.close();
+            clientSocket.close();
+        }
+        catch(IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
+    private void uploadFile(String filename)
+    {
+        try {
+            File file = new File(filename);
+            if(!file.exists())
+            {
+                file.createNewFile();
+            }
+            String line = null;
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+
+            while((line = input.readLine()) != null)
+            {
+                writer.write(line);
+                writer.newLine();
+            }
+            input.close();
+            writer.close();
+            clientSocket.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    private void downloadFile(String filename)
+    {
+        try {
+            input.close();
+            File file = new File(filename);
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream());
+            Scanner scan = new Scanner(file);
+
+            while(scan.hasNextLine())
+            {
+                out.println(scan.nextLine());
+            }
+
+            out.close();
+            scan.close();
+            clientSocket.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
--- src/server/Server.java
+++ src/server/Server.java
@@ -5,114 +5,64 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Server
+public class Server implements Runnable
 {
-    public static final String DIR = "dir";
-    public static final String UPLOAD = "upload";
-    public static final int UPLOAD_PORT = 8081;
-    public static final String DOWNLOAD = "download";
-    public static final int DOWNLOAD_PORT = 8008;
-
     private ServerSocket serverSocket = null;
+    private Socket connectionBuffer = null;
+    private boolean running = true;
+    private File sharedFolder;
 
-    public Server(int port) {
-        try {
+    public Server(int port, String folderName)
+    {
+        try
+        {
             serverSocket = new ServerSocket(port);
-        } catch (IOException e) {
+
+            sharedFolder = new File(folderName);
+
+            if(!sharedFolder.exists())
+                sharedFolder.mkdir();
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
 
-    public void handleRequests() {
-        System.out.println("Server Listening");
-
-        try {
-            File file = new File("test_uploaded.txt");
-            file.createNewFile();
-
-            String line = null;
-            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-            Socket clientSocket = serverSocket.accept();
-
-            BufferedReader in = new BufferedReader(
-                    new InputStreamReader(
-                            clientSocket.getInputStream()
-                    )
-            );
-/*
-            String line = null;
-            String filename = "text.txt";
-
-            while ((line = in.readLine()) != null) {
-                String[] text = line.split(" ");
-                break;
-            }
-
-            // copy the data from the file to the socket
-            FileInputStream fileIn = new FileInputStream(filename);
-            OutputStream out = clientSocket.getOutputStream();
-            //uploadFile(filename, out);
-            copyAllText(fileIn, out);
-
-            // close everything down
-            fileIn.close();
-            in.close();
-            out.close();
-            clientSocket.close();*/
-
-
-            while((line = in.readLine()) != null) {
-                writer.write(line);
-                writer.newLine();
-            }
-            writer.close();
-            clientSocket.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+    @Override
+    public void run()
+    {
+        while(running)
+        {
+            waitForConnections();
         }
-
     }
 
-    private void copyAllText(FileInputStream in, OutputStream out)
-            throws IOException {
-        byte[] buffer = new byte[1024];
-        int numBytes = -1;
-        while ((numBytes = in.read(buffer)) > 0) {
-            out.write(buffer);
+
+    private void waitForConnections()
+    {
+        while(true)
+        {
+            try
+            {
+                System.out.println("Waiting for Connections");
+                connectionBuffer = serverSocket.accept();
+
+                System.out.println("Connection Established");
+                ClientThread newConnection = new ClientThread(connectionBuffer, sharedFolder);
+            }
+            catch(IOException ioe)
+            {
+                ioe.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args)
     {
-        Server server = new Server(8080);
-        server.handleRequests();
-
-    }
-
-
-    public static void displayDirectory()
-    {
-
-    }
-    public static void uploadFile(String filename, OutputStream out)
-    {
-
-    }
-    public static void downloadFile(String filename)
-    {
-
+        Server server = new Server(8080, "Server-Shared");
+        server.run();
     }
 }
-
-//main/listener thread
-
-//display directory thread
-
-//upload thread
-
-//download thread
-
 
 
